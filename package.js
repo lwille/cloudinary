ecbdf067050fec5be70f12086a0ce84ab8c0703e
Leonhardt Wille <leonhardt.wille@erasys.de>
2013-11-18 20:49:09 +0100

    renamed client submodule

--- package.js
+++ package.js
@@ -4,8 +4,8 @@ Package.describe({
 
 Package.on_use(function (api) {
   api.use('jquery', 'client');
-  api.add_files('lib/js/jquery.ui.widget.js', 'client');
-  api.add_files('lib/js/jquery.iframe-transport.js', 'client');
-  api.add_files('lib/js/jquery.fileupload.js', 'client');
-  api.add_files('lib/js/jquery.cloudinary.js', 'client');
+  api.add_files('client/js/jquery.ui.widget.js', 'client');
+  api.add_files('client/js/jquery.iframe-transport.js', 'client');
+  api.add_files('client/js/jquery.fileupload.js', 'client');
+  api.add_files('client/js/jquery.cloudinary.js', 'client');
 });
